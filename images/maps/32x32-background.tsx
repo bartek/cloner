bfb94decbcc5b836b3ce998c5fd414468339865d
Corey Dutson <coreyd@gadventures.com>
2013-10-01 20:34:11 -0400

    Added more tiles, the template files I'm using, and updated the tutorial map to be better

---
+++ images/maps/32x32-background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="32x32-functional" tilewidth="32" tileheight="32">
+ <image source="../32x32-functional.png" width="640" height="640"/>
+</tileset>
